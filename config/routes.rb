e53fb0fa62b793cf6d94b29f3adbbf47314446ff
Julia Thea <yuliya.boyadjan@gmail.com>
2020-05-11 17:14:11 -0500

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+  root to: 'cocktails#index'
+  get 'cocktails/index', to: 'cocktails#index'
+  get 'cocktails/show'
+  get 'cocktails/new'
+  get 'cocktails/post'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  resources :maps
 end
